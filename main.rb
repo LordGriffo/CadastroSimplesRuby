978a565fdbd7322b652b80b572dd37bee629a260
Olavo Borges <120344319+LordGriffo@users.noreply.github.com>
2022-12-12 01:27:03 -0300

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,44 @@
+#Simples script de registro em Ruby
+#Desmarque a opção referente ao sistema que for testar
+#script feito em um sábado assistindo MY da MTV
+#somente para brincar hahah
+#Ruby, Ruby, Não Rails !
+
+while true do
+
+puts "Cadastro de fluxo de pessoas"
+puts
+puts "Nome:"
+@nome = gets.chomp
+puts "Idade:"
+@idade = gets.chomp
+puts "RG:"
+@rg = gets.chomp
+puts "Cidade:"
+@cidade = gets.chomp
+sleep 0.1
+#system("cls") 		#para Windows
+#system("clear")  	#para Linux
+
+def dados
+arquivo = []
+arquivo << puts
+arquivo << "#{Time.new}"  
+arquivo << "Nome: #{@nome}".upcase
+arquivo << "Idade: #{@idade}".upcase
+arquivo << "RG: #{@rg}".upcase
+arquivo << "Cidade: #{@cidade}".upcase
+arquivo << "********************"
+arquivo << puts
+end
+
+x = File.open("Dados.txt", "a+")
+x.puts(dados)
+x.close
+
+puts "Dados registrados:"
+puts dados
+sleep 2
+#system("cls")
+#system("clear")
+end
